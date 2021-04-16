6a57013bd9bd2125a1bc14019b6f5686f1a10cf4
Ara Abovyan <araabovyan@gmail.com>
2021-04-16 20:12:50 +0400

    'v0'

---
+++ Bank_management_system/src/main_page.java
@@ -0,0 +1,8 @@
+
+public class main_page {
+	public static void main(String[] str) {
+		Customer c1 = new Customer(1, "John", "Snow", "johnsnow@gmail.com", "+374123549", "john49snow", "280AC", "Winterfell", "JOHN");
+		c1.print_customer();
+
+	}
+}
